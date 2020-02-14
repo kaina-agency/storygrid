b54f71fc55bd281784649752728ca04bd0a896d5
James Lovallo <james.lovallo@mrm-mccann.com>
2020-02-14 12:25:51 -0500

    [skip ci] feature: this image component is so hot

--- src/components/ImageG.vue
+++ src/components/ImageG.vue
@@ -22,28 +22,39 @@
 		computed: {
 			src() {
 				let b = this.blok;
-				let [sizes, srcset, jpegSrcset, src] = "";
+				let bo = String(b.options);
+				let [sizes, srcset, jpegSrcset, src, c, pt] = "";
 				let sb = "//a.storyblok.com";
 				let ba = b.aspect_ratio || "16/9";
-				let r = ba.split("/")[1] / ba.split("/")[0];
-				let contain = b.options.includes("contain") ? "contain" : "cover";
-				let pt = `padding-top: ${r * 100}%;`;
+				let br = ba.split("/")[1] / ba.split("/")[0];
+				let bpt = `padding-top: ${br * 100}%;`;
+				let bv = b.vertical_alignment || 5;
+				let bh = b.horizontal_alignment || 5;
+				let keepAspect = bo.includes("keep_aspect");
+				let contain = bo.includes("contain") ? "contain" : "cover";
+				pt = bpt;
 
 				if (b.image.includes(sb)) {
 					let cdn = "https://img2.storyblok.com";
-					let s = b.options.includes("smart") ? "/smart" : "";
+					let s = bo.includes("smart") ? "/smart" : "";
 					let q = `/filters:quality(${b.quality || "50"})`;
 					let f = ":format(webp)";
 					let i = b.image.replace(sb, "");
+					let ia = String(i.match(/\d+x\d+/g));
+					let ir = ia.split("x")[1] / ia.split("x")[0];
+					let ipt = `padding-top: ${ir * 100}%;`;
+					pt = bo.includes("natural") ? ipt : bpt;
+
 					let bps = [400, 800, 1200, 1600];
 
 					bps.forEach(bp => {
-						let w = b.options.includes("large") ? bp : Math.floor(bp / 2);
-						let h = b.options.includes("keep_aspect") ? "" : Math.floor(w * r);
+						let w = b.high_quality ? bp : Math.floor(bp / 2);
+						let h = keepAspect ? "" : Math.floor(w * br);
 						let d = `/${w}x${h}`;
+						c = s ? "" : `:focal(${0}x${h * bv}:${w * bh}x${0})`;
 
 						sizes += `(max-width: ${bp}px) ${bp}w, `;
-						srcset += cdn + d + s + q + f + i + ` ${bp}w, `;
+						srcset += cdn + d + s + q + f + c + i + ` ${bp}w, `;
 						jpegSrcset += cdn + d + s + q + i + ` ${bp}w, `;
 					});
 
