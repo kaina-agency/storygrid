dab561ae03c23fa9a8a37efe8f0fd96c3becbb2f
James Lovallo <james.lovallo@mrm-mccann.com>
2020-02-20 10:30:18 -0500

    [skip ci] feature: project switching fixes

--- src/.temp/plugins-server.js
+++
@@ -1,8 +0,0 @@
-import plugin_gridsome_source_storyblok_5 from "/Users/james.lovallo/Documents/GitHub/gridsome-test/node_modules/gridsome-source-storyblok/gridsome.client.js"
-
-export default [
-  {
-    run: plugin_gridsome_source_storyblok_5,
-    options: {"client":{"accessToken":"f2u4dLnrVwVuhL9rU6PvSgtt"}}
-  }
-]
