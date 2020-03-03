9d64ec7b5e506b6d46702a72c3d89df93869b096
James Lovallo <james.lovallo@mrm-mccann.com>
2020-03-03 15:03:43 -0500

    [skip ci] feature: added page-level css & html overrides

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,12 +1,14 @@
-<template>
-	<div>
-		<component
+<template lang="pug">
+	div
+		component(
 			:key="blok._uid"
 			v-for="blok in blok.body"
 			:blok="blok"
 			:is="blok.component"
-		></component>
-	</div>
+		)
+		div(v-html="blok.inject_html")
+		component(:is="'style'")
+			| {{ blok.inject_css }}
 </template>
 
 <script>
