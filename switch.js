eecf35163f2636959b347b954a316a8eba12797e
James Lovallo <info@patrickonparker.com>
2020-04-22 16:55:02 -0400

    [skip ci] feature: remove env file

--- switch.js
+++ switch.js
@@ -1,35 +1,13 @@
 const download = require('download-file')
-const os = require('os')
-const fs = require('fs')
 const icon = process.env.ICON
 
-// update .env file
-
-let sb = 'GRIDSOME_SB=' + process.env.GRIDSOME_SB + os.EOL
-let t = 'GRIDSOME_TITLE=' + process.env.GRIDSOME_TITLE + os.EOL
-
-let content = sb + t
-
-fs.writeFile('./.env', content, (err) => {
-	if (err) {
-		console.error(err)
-	} else {
-		console.log(
-			'\n\n',
-			'.env file written from Netlify build settings'
-		)
-	}
-})
-
 // update icon
-
 var icon_options = {
 	directory: './src/',
 	filename: 'icon.png',
 }
-
 if (icon) {
-	download(icon, icon_options, function (err) {
+	download(icon, icon_options, function(err) {
 		if (err) {
 			console.error(err)
 		} else {
