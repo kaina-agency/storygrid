a6e0ed57e895079cd19e9853d579e750ee152a9b
James Lovallo <patrickonparker@gmail.com>
2020-08-31 12:56:26 -0400

    [skip ci] feature: add Google Meet support to calendar events

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -4,22 +4,21 @@
 	v-dialog(v-model="popup", width="500")
 		v-card.event
 			v-card-text.description
-				p.title.bold {{ selectedEvent.title }}
+				p.title.bold {{ selectedEvent.summary }}
 				p.mb-0(v-html="allDay(selectedEvent.start, selectedEvent.end)")
 				.d-flex.mb-4(v-if="selectedEvent.location")
-					v-icon(left)
-						| M12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5M12,2A7,7 0 0,1 19,9C19,14.25 12,22 12,22C12,22 5,14.25 5,9A7,7 0 0,1 12,2M12,4A5,5 0 0,0 7,9C7,10 7,12 12,18.71C17,12 17,10 17,9A5,5 0 0,0 12,4Z
+					v-icon(left) M12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5M12,2A7,7 0 0,1 19,9C19,14.25 12,22 12,22C12,22 5,14.25 5,9A7,7 0 0,1 12,2M12,4A5,5 0 0,0 7,9C7,10 7,12 12,18.71C17,12 17,10 17,9A5,5 0 0,0 12,4Z
 					a(
 						:href="`https://maps.google.com/maps?hl=en&q=${encodeURI(selectedEvent.location)}`"
-					)
-						| {{ selectedEvent.location }}
+					) {{ selectedEvent.location }}
+				.d-flex.mb-4(v-if="selectedEvent.hangoutLink")
+					v-icon(left) M15,11L14,13H12.5L13.5,11H12V8H15M11,11L10,13H8.5L9.5,11H8V8H11M11.5,2A8.5,8.5 0 0,0 3,10.5A8.5,8.5 0 0,0 11.5,19H12V22.5C16.86,20.15 20,15 20,10.5C20,5.8 16.19,2 11.5,2Z
+					a(:href="selectedEvent.hangoutLink") Join on Google Meet
 				.d-flex(v-if="selectedEvent.description")
-					v-icon(left)
-						| M21,6V8H3V6H21M3,18H12V16H3V18M3,13H21V11H3V13Z
+					v-icon(left) M21,6V8H3V6H21M3,18H12V16H3V18M3,13H21V11H3V13Z
 					p(v-html="selectedEvent.description")
 				.d-flex(v-if="selectedEvent.attachments")
-					v-icon.mt-1(left)
-						| M7.71,3.5L1.15,15L4.58,21L11.13,9.5M9.73,15L6.3,21H19.42L22.85,15M22.28,14L15.42,2H8.58L8.57,2L15.43,14H22.28Z
+					v-icon.mt-1(left) M7.71,3.5L1.15,15L4.58,21L11.13,9.5M9.73,15L6.3,21H19.42L22.85,15M22.28,14L15.42,2H8.58L8.57,2L15.43,14H22.28Z
 					.d-flex.flex-wrap
 						v-chip.mr-2.mb-2.attachment(
 							v-for="attachment in selectedEvent.attachments",
@@ -110,14 +109,7 @@
 
 				console.log(event);
 
-				this.selectedEvent = {
-					title: event.summary,
-					description: event?.description,
-					location: event?.location,
-					start: event?.start,
-					end: event?.end,
-					attachments: event?.attachments,
-				};
+				this.selectedEvent = event;
 
 				this.popup = true;
 			},
@@ -156,6 +148,11 @@
 		.attachment {
 			max-width: calc(100vw - 130px);
 		}
+	}
+
+	.fc-day-past,
+	.fc-day-past + .fc-event {
+		opacity: 0.66;
 	}
 
 	.fc-toolbar {
